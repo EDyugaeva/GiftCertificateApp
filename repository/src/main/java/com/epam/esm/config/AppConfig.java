b258006743363e4e4a713adfe918cfd55eddad72
EDyugaeva <ed2408@yandex.ru>
2023-12-08 18:00:10 +0500

    Refactoring: adding profiles

--- repository/src/main/java/com/epam/esm/config/AppConfig.java
+++ repository/src/main/java/com/epam/esm/config/AppConfig.java
@@ -1,7 +1,6 @@
 package com.epam.esm.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -13,6 +12,7 @@ import javax.sql.DataSource;
 @ComponentScan("com.epam.esm")
 @PropertySource(value = "classpath:application-${spring.profiles.active}.properties")
 @Slf4j
+@Profile({"dev", "prod"})
 public class AppConfig {
     @Value("${database.driver.class.name}")
     private String driverClassName;
@@ -22,24 +22,12 @@ public class AppConfig {
     private String username;
     @Value("${database.password}")
     private String password;
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
 
     @Bean
-    @Profile("prod")
     public DataSource dataSource() {
-        log.info("Active profile is prod");
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(driverClassName);
-        dataSource.setUrl(url);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
-        return dataSource;
-    }
-
-    @Bean
-    @Profile("dev")
-    @Qualifier("dataSource")
-    public DataSource dataSourceDev() {
-        log.info("Active profile is dev");
+        log.info("Active profile is {}", activeProfile );
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(driverClassName);
         dataSource.setUrl(url);
